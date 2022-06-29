23e09557a73b8f9446ca7612e2a5799ddd640962
Eddie Villalba <eddiejv@google.com>
2022-06-29 11:35:58 -0500

    first commit

---
+++ src/middleware/upload.js
@@ -0,0 +1,9 @@
+const util = require("util");
+const Multer = require("multer");
+const maxSize = 200 * 1024 * 1024;
+let processFile = Multer({
+    storage: Multer.memoryStorage(),
+    limits: { fileSize: maxSize },
+}).single("file");
+let processFileMiddleware = util.promisify(processFile);
+module.exports = processFileMiddleware;
